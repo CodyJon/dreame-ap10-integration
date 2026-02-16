9bb5553e86b99de72215366a3201cf5024fab6c0
CodyJon <122034899+CodyJon@users.noreply.github.com>
2026-02-16 12:50:10 -0700

    Delete custom_components/dreame_airpurifier directory

--- custom_components/dreame_airpurifier/const.py
+++
@@ -1,9 +0,0 @@
-"""Constants for Dreame Air Purifier integration."""
-
-DOMAIN = "dreame_airpurifier"
-SCAN_INTERVAL = 30
-
-CONF_COUNTRY = "country"
-COUNTRY_OPTIONS = ["us", "cn", "eu", "sg", "kr"]
-
-PRESET_MODES = ["Auto", "Sleep", "Custom", "Pet"]
